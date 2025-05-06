ddefa5b4fc98958e9b794130e9111d3abd0a5c93
Until <kostyuchenko.work@gmail.com>
2025-05-06 21:18:42 +0300

    .

--- task/update_statistics.py
+++ task/update_statistics.py
@@ -1,11 +1,16 @@
 from celery import Celery
 import redis
+from settings.config import settings
 
-celery = Celery('tasks', broker='redis://redis:6379/0')
+# Инициализация Celery
+celery = Celery('tasks', broker=settings.get_redis_url())
 
 @celery.task
 def update_statistics(transaction_data):
-    r = redis.Redis(host='redis', port=6379, db=0)
+    # Подключение к Redis
+    r = redis.Redis.from_url(settings.get_redis_url())
+    
+    # Обновление статистики
     r.incr("stats:total_transactions")
     amount = transaction_data["amount"]
     r.incrbyfloat("stats:total_amount", amount)
